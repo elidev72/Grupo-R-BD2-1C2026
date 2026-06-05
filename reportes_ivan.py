2ae758a353e2e91ab3ee1958fa44c4f2e1ff38ad
ivan <ivaninfonet@gmail.com>
2026-06-04 23:20:09 -0300

    Avances reportes MongoDB

--- reportes_ivan.py
+++ reportes_ivan.py
@@ -53,13 +53,83 @@ def reporte_1(fecha_desde, fecha_hasta):
 # recomendacion hagan la consulta primero en el mongodb compass o lo que usen para
 # abstraerse de problemas de codigo y una vez la tienen la intentan agregar al codigo
 
+
+
+
+
 #
 #2. Un reporte con las cantidades de ventas agrupadas por obras sociales y además considerar
 #los privados (sin obra social) como un grupo mas. Todo esto debe ocurrir entre dos fechas
 #pasadas como parámetros (fecha desde y fecha hasta)
 
+
+
 def reporte_2(fecha_desde, fecha_hasta):
-    pass # nota borrar el pass en la funciones y colocar la consulta
+    collection = Venta._get_collection()
+
+    pipeline = [
+        # 1. filtro por fechas
+        {
+            "$match": {
+                "fecha": {
+                    "$gte": fecha_desde,
+                    "$lte": fecha_hasta
+                }
+            }
+        },
+
+        # 2. join con personas
+        {
+            "$lookup": {
+                "from": "personas",
+                "localField": "cliente",
+                "foreignField": "_id",
+                "as": "cliente_info"
+            }
+        },
+
+        # 3. unwind cliente
+        {
+            "$unwind": "$cliente_info"
+        },
+
+        # 4. join obra social
+        {
+            "$lookup": {
+                "from": "obra_social",  # ojo: puede variar el nombre
+                "localField": "cliente_info.afiliacion.obra_social",
+                "foreignField": "_id",
+                "as": "obra_social_info"
+            }
+        },
+
+        # 5. unwind obra social (para poder agrupar bien)
+        {
+            "$unwind": {
+                "path": "$obra_social_info",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+
+        # 6. group final
+        {
+            "$group": {
+                "_id": {
+                    "$ifNull": [
+                        "$obra_social_info.nombre",
+                        "Privado"
+                    ]
+                },
+                "cantidadVentas": {
+                    "$sum": 1
+                }
+            }
+        }
+    ]
+
+    return list(collection.aggregate(pipeline))
+
+
 
 #
 #6. Ranking de cantidad de productos vendidos, agrupado por producto y por sucursal.
